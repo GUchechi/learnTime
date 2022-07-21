f32462dbdce4c77c9361508edc90bfd48e5c6e64
GUchechi <godswilluchendu7@gmail.com>
2022-07-21 09:13:18 -0700

    imported swiper/css

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './Projects.css';
+import {Swiper, SwiperSlide} from 'swiper/react'
+import Sidebar from '../../img/sidebar.png'
+import Ecommerce from '../../img/ecommerce.png'
+import HOC from '../../img/hoc.png'
+import MusicApp from '../../img/musicapp.png'
+import 'swiper/css'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+        {/* Heading */}
+        <span>Recent Projects
+            <br/>
+            our students.
+        </span>
+        <span>Projects</span>
+
+         {/* Slider */}
+         <Swiper>
+            <SwiperSlide>
+
+            </SwiperSlide>
+         </Swiper>
+    
+
+    </div>
   )
 }
 

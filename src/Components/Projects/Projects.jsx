9bfe3f60a5a6f832d2f024d5a86649f04c494390
GUchechi <godswilluchendu7@gmail.com>
2022-07-21 10:15:02 -0700

    imported images to my project component

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Projects.css';
 import {Swiper, SwiperSlide} from 'swiper/react'
-import Sidebar from '../../img/sidebar.png'
-import Ecommerce from '../../img/ecommerce.png'
-import HOC from '../../img/hoc.png'
-import MusicApp from '../../img/musicapp.png'
+import Project1 from '../../img/Project1.png'
+import Project2 from '../../img/Project2.png'
+import Project3 from '../../img/Project3.png'
+import Project4 from '../../img/Project4.png'
+import Project5 from '../../img/Project5.png'
+import Project6 from '../../img/Project6.png'
+import Project7 from '../../img/Project7.png'
+import Project8 from '../../img/Project8.png'
+import Project9 from '../../img/Project9.png'
+import Project10 from '../../img/Project10.png'
 import 'swiper/css'
 
 const Projects = () => {
@@ -20,13 +26,34 @@ const Projects = () => {
          {/* Slider */}
          <Swiper>
             <SwiperSlide>
-                <img src={Sidebar} alt="" />
+                <img src={Project1} alt="" />
             </SwiperSlide>
             <SwiperSlide>
-                <img src={Ecommerce} alt="" />
+                <img src={Project2} alt="" />
             </SwiperSlide>
             <SwiperSlide>
-                <img src={HOC} alt="" />
+                <img src={Project3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project4} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project5} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project6} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project7} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project8} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project9} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Project10} alt="" />
             </SwiperSlide>
          </Swiper>
     

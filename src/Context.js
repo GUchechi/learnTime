49834c3c7bdaa56dd38d7335ef7825a348900eed
GUchechi <godswilluchendu7@gmail.com>
2022-07-23 02:54:45 -0700

    made the toggle functional

--- src/Context.js
+++ src/Context.js
@@ -2,7 +2,7 @@ import {createContext, useReducer} from 'react'
 
 export const themeContext = createContext();
 
-const initialState = {darkMode : true};
+const initialState = {darkMode : false};
 
 const themeReducer =(state, action) => {
     switch (action.type) {
@@ -13,11 +13,11 @@ const themeReducer =(state, action) => {
     }
 };
 
-export const ThemeProvider = (props) => {
+export const ThemeProvider = ({children}) => {
     const [state, dispatch] = useReducer (themeReducer, initialState);
     return (
         <themeContext.Provider value={{state, dispatch}}>
-            {props.children}
+            {children}
         </themeContext.Provider>
     )
 }
